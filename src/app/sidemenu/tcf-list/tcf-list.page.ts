70c5744f58897e4de1cd865060e73fe8eedec9bc
Satish <msatish080@gmail.com>
2024-08-05 11:31:01 +0530

    tcf list changes

--- src/app/sidemenu/tcf-list/tcf-list.page.ts
+++ src/app/sidemenu/tcf-list/tcf-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tcf-list',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tcf-list.page.scss'],
 })
 export class TcfListPage implements OnInit {
-
-  constructor() { }
+  selectedTime: string = '';
+  isTimePickerOpen = false;
+  constructor(private navController:NavController) { }
 
   ngOnInit() {
   }
+  openTimePicker() {
+    this.isTimePickerOpen = true;
+  }
 
+  closeTimePicker() {
+    this.isTimePickerOpen = false;
+  }
+
+  timeChanged(event: any) {
+    this.selectedTime = event.detail.value;
+    console.log('Selected selectedTime:', this.selectedTime);
+  }
+
+  clearTime() {
+    this.selectedTime = '';
+    this.closeTimePicker();
+  }
+  gotoaddtcfpage(){
+    this.navController.navigateRoot("/add-tcf");
+  }
 }
